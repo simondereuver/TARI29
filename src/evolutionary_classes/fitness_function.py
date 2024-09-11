a744f78e5d4ef187424a2a3d03a8779b78f24b01
ivonilsson <ivo_gote@hotmail.com>
2024-09-11 16:05:12 +0200

    Some fixes and edge case handling

--- src/evolutionary_classes/fitness_function.py
+++ src/evolutionary_classes/fitness_function.py
@@ -2,6 +2,7 @@
 Module containing fitness function(s)
 """
 
+import warnings
 from typing import List
 import numpy as np
 
@@ -9,35 +10,50 @@ class FitnessFunction:
     """FitnessFunction class definiton"""
     def __init__(self, graph: np.ndarray, bounds: tuple = None):
         """Fitness function init"""
-        self.graph = graph
-        self.bounds = bounds
+        if graph is not None:
+            self.graph = graph
+        else:
+            raise RuntimeError("No graph. Exiting...")
+        if bounds is not None:
+            self.bounds = bounds
+        else:
+            warnings.warn("No boundaries set. FitnessFunction.bound is None", RuntimeWarning)
 
-    def fitness_function_distance_based(self, route: List) -> int:
-        """Distance based fitness function"""
-        # evaluates the fitness score based on the total_distance. The lower the value, the better.
+    def _calculate_distance(self, route: List) -> int:
+        """Helper function for calculating the distance"""
+        if route is None or len(route) < 2:
+            raise ValueError("The provided route is less than 2. Exiting...")
+
         x = 0
 
         for i in range(len(route) - 1):
             x += self.graph[route[i]][route[i + 1]]
 
+        x += self.graph[route[-1]][route[0]]
+
         return x
+
+    def fitness_function_distance_based(self, route: List) -> int:
+        """Distance based fitness function"""
+        # evaluates the fitness score based on the total_distance. The lower the value, the better.
+        return self._calculate_distance(route)
 
     def fitness_function_normalized(self, route: List) -> float:
         """Normalized version"""
         # if there are no bounds, then we evaluate the fitness score as 1 / total_distance
         if self.bounds is None:
-            x = 0
-            for i in range(len(route) - 1):
-                x += self.graph[route[i]][route[i + 1]]
-
-            return 1/x
+            return 1 / self._calculate_distance(route)
 
         # else we normalize the value with the bounds as
         # (higher - total_distance) / (higher - lower),
         # where the fitness score is represented as a float from 0 to 1 where 1 is a perfect fit
-        x = 0
-        for i in range(len(route) - 1):
-            x += self.graph[route[i]][route[i + 1]]
+        x = self._calculate_distance(route)
 
         lower, higher = self.bounds
+
+        if higher is None:
+            warnings.warn("Only a lower bound was provided. Returning lower_bound / total_distance",
+                           RuntimeWarning)
+            return lower / x
+
         return (higher - x) / (higher - lower)
