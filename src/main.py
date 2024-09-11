c3cced72c0ddbec571648e2ae2e33d80d0cbafb2
Simon de Reuver <dereuversimon@gmail.com>
2024-09-11 18:16:57 +0200

    add 1-tree for calculating lowerbound

--- src/main.py
+++ src/main.py
@@ -9,12 +9,27 @@ EDGE_WEIGHT_SPAN = (1, 25)
 
 g = Graph(NUMBER_OF_NODES, EDGE_WEIGHT_SPAN)
 
-graph = g.generate_random_graph(seed=1)
+graph = g.generate_random_graph(i)
 
 print(graph)
 
 shortest_path, weight = g.solve_bf(graph, 0)
 
-print(f"Shortest path: {shortest_path}, {weight} meters")
+lowerbound1 = g.max_one_tree_lower_bound(graph)
+
+print(f"Shortest path: {shortest_path}, {weight} meters, Lowerbound_1_tree: {lowerbound1}")
 
 g.show_graph(graph)
+
+# Example usage to run the 1-tree on 10 different graphs
+#for i in range(10):
+#    graph = g.generate_random_graph(i)
+#
+#    print(graph)
+
+#    shortest_path, weight = g.solve_bf(graph, 0)
+
+    #lowerbound = g.lower_bound(graph)
+#    lowerbound1 = g.max_one_tree_lower_bound(graph)
+
+#    print(f"Shortest path: {shortest_path}, {weight} meters, Lowerbound_1_tree: {lowerbound1}")
