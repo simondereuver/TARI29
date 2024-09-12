dbe11fb301216fd91ac439c9038927e38abd6328
Parna Saeidpour <parnasaeidpour@gmail.com>
2024-09-12 14:31:11 +0200

    Pick fix size population from nodes

--- src/evolutionary_classes/population_search.py
+++ src/evolutionary_classes/population_search.py
@@ -1,3 +1,12 @@
 """
 Module containing population-based search
 """
+import random
+
+class RandomPopulation:
+    def __init__(self, nodes, size) -> None:
+        self.values =[random.sample(nodes, len(nodes)) for _ in range(size)]
+
+    def get_values(self):
+        return self.values
+
