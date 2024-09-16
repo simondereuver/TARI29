10252bbf073af201351a365bbdee5f9a6bca70f0
Parna Saeidpour <parnasaeidpour@gmail.com>
2024-09-16 20:35:34 +0200

    Creating random population without duplication

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@
 
 # example code for generating random graphs
 from main_classes.graph import Graph
+from evolutionary_classes.population_search import RandomPopulation
 
 NUMBER_OF_NODES = 5
 EDGE_WEIGHT_SPAN = (1, 25)
@@ -20,6 +21,15 @@ lowerbound1 = g.max_one_tree_lower_bound(graph)
 print(f"Shortest path: {shortest_path}, {weight} meters, Lowerbound_1_tree: {lowerbound1}")
 
 g.show_graph(graph)
+
+
+# Generate a population of random node permutations
+population_size = 5  
+random_population = RandomPopulation(nodes=list(range(NUMBER_OF_NODES)), size=population_size, seed=42)
+
+node_permutations = random_population.get_values()
+
+print(f"Generated Node Permutations: {node_permutations}")
 
 # Example usage to run the 1-tree on 10 different graphs
 #for i in range(10):
