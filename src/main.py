0fd6bea549c6c035c5ca79651d3f12bc27a81ba3
Simon de Reuver <dereuversimon@gmail.com>
2024-09-17 17:28:47 +0200

    Add simple usage of evolutionary algorithm in main.py file

--- src/main.py
+++ src/main.py
@@ -3,8 +3,11 @@
 
 # example code for generating random graphs
 from main_classes.graph import Graph
+from evolutionary_classes.selection import Selection
+#from evolutionary_classes.fitness_function import FitnessFunction
+from evolutionary_classes.population import Population
 
-NUMBER_OF_NODES = 5
+NUMBER_OF_NODES = 10
 EDGE_WEIGHT_SPAN = (1, 25)
 
 g = Graph(NUMBER_OF_NODES, EDGE_WEIGHT_SPAN)
@@ -19,9 +22,29 @@ lowerbound1 = g.max_one_tree_lower_bound(graph)
 
 print(f"Shortest path: {shortest_path}, {weight} meters, Lowerbound_1_tree: {lowerbound1}")
 
-g.show_graph(graph)
+#g.show_graph(graph)
 
-# Example usage to run the 1-tree on 10 different graphs
+population_mngr = Population()
+selection_mngr = Selection()
+initial_population = population_mngr.initial_population(graph)
+
+NUM_GENERATIONS = 100
+curr_gen = initial_population
+
+#run for a set of generations
+for generation in range(NUM_GENERATIONS):
+    curr_gen = population_mngr.gen_new_population(graph, curr_gen, selection_mngr)
+
+if not curr_gen:
+    print("\nNo valid paths found in the final generation.")
+else:
+    best_path = min(curr_gen, key=lambda path: selection_mngr.distance(graph, path))
+    best_distance = selection_mngr.distance(graph, best_path)
+    # + [0] is a quick fix for now to make it look similar to the solution in the bruteforce
+    print(f"\nBest path found: {best_path + [0]} with distance: {best_distance}")
+
+
+#Example usage to run the 1-tree on 10 different graphs
 #for i in range(10):
 #    graph = g.generate_random_graph(i)
 #
